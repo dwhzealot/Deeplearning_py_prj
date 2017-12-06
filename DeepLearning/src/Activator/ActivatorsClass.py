69908b59d71e3d83ca3638ba3fafb4db879aea20
dwhzealot <33685532+dwhzealot@users.noreply.github.com>
2017-12-06 17:41:30 +0800

    Update ActivatorsClass.py

--- DeepLearning/src/Activator/ActivatorsClass.py
+++ DeepLearning/src/Activator/ActivatorsClass.py
@@ -2,25 +2,22 @@
 import numpy as np
 
 class SigmoidActivator(object):
-    def forward(self, weighted_input):
-        return 1.0 / (1.0 + np.exp(-weighted_input))
-    def backward(self, output):
-        return output * (1 - output)
+    def forward(self, Z):
+        return 1.0 / (1.0 + np.exp(-Z))
+    def backward(self, Z):
+        return Z * (1 - Z)
 
 class TanhActivator(object):
-    def forward(self, weighted_input):
-        return (np.exp(weighted_input) - np.exp(-weighted_input)) / (np.exp(weighted_input) + np.exp(-weighted_input))
-    def backward(self, output):
-        return (1 -np.square(output))
+    def forward(self, Z):
+        return (np.exp(Z) - np.exp(-Z)) / (np.exp(Z) + np.exp(-Z))
+    def backward(self, Z):
+        return (1 -np.square(Z))
     
 class ReLUActivator(object):
-    def forward(self, weighted_input):
-        if weighted_input > 0 :
-            return weighted_input
-        else :
-            return 0
-    def backward(self, output):
-        if output > 0 :
-            return 1
-        else :
-            return 0
+    def forward(self, Z):
+        return np.maximum(Z, 0)
+    
+    def backward(self, Z):
+        Z = np.maximum(0,Z)
+        Z[Z > 0] = 1
+        return Z
