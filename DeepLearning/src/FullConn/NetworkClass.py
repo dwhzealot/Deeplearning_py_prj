e4da647b321f3457a71574082dc5dd78b56a589c
dwhzealot <33685532+dwhzealot@users.noreply.github.com>
2017-12-08 14:36:56 +0800

    Update NetworkClass.py

--- DeepLearning/src/FullConn/NetworkClass.py
+++ DeepLearning/src/FullConn/NetworkClass.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 import numpy as np
+from CostF import *
 
 class FullConnLayer(object):
     def __init__(self, m, W_ini_coe ,activator, learn_rate, is_OutputLayer, n,column):
@@ -41,6 +42,7 @@ class FullConnLayer(object):
         W_next_layer: 下一层的权重W
         Y: 标签值
         '''
+
         if (self.is_OutputLayer == True):
             self.dZ = self.A - Y
         else:
@@ -56,14 +58,13 @@ class FullConnLayer(object):
 class FullConnNetwork (object):
     def __init__(self, m, s, l_max, W_ini_coe ,activator, learn_rate,n):
         '''
-        (self, m, n, W_ini_coe ,activator, learn_rate, column)
         m     :样本个数
         s     :单样本元素个数
         l_max : 总层数
         W_ini_coe: W初始化系数
         activator: 激活函数
         learn_rate: W, b更新时的学习率 
-        n: 每层神经元个数
+        n: 记录每层的神经元个数的数组
         '''
         self.m = m
         self.s = s
@@ -72,7 +73,7 @@ class FullConnNetwork (object):
         is_outputlayer = False
         column = 0       
         assert(len(n) == l_max)
-        
+        self.n = n
         if l_max == 1:
             layer = FullConnLayer(m, W_ini_coe ,activator, learn_rate, True, n[0], s)    
             self.layer.append(layer)
@@ -106,4 +107,51 @@ class FullConnNetwork (object):
     def Train(self, X, Labels):
         self.ForwardPropagation(X)
         self.BackwardPropagation(Labels)
-            
+    
+    def GradientCheck (self):
+        '''
+        #在梯度检查中，如果每次反向传播不更新W， 计算出的delta会比较小。此函数还是保留了剃度检查中的W的更细                                 
+        '''
+        CostF = CrossEntropy()
+        assert(self.n[self.l_max - 1] == 1)
+        X = [1,2,3,4,5]
+        X = np.reshape(X, (1,5))
+        Y = [0.1,0.2,0.3,0.4,0.5]
+        Y = np.reshape(Y, (1,5))
+        delta_max = 0
+        epsilon = 1e-07
+        for l in range(self.l_max):
+            for i in range(self.layer[l].W.shape[0]):
+                for j in range(self.layer[l].W.shape[1]):
+                    W_backup = np.copy(self.layer[l].W)
+                    W_add_epsilon = np.copy(self.layer[l].W)
+                    W_minus_epsilon = np.copy(self.layer[l].W)
+                    W_add_epsilon[i,j] += epsilon
+                    W_minus_epsilon[i,j] -= epsilon
+                    
+                    self.layer[l].W = W_add_epsilon
+                    A_W_add_epsilon = self.ForwardPropagation(X)
+                    L_W_add_epsilon = CostF.Calculate(A_W_add_epsilon, Y)
+
+                    self.layer[l].W = W_minus_epsilon
+                    A_W_minus_epsilon = self.ForwardPropagation(X)
+                    L_W_minus_epsilon = CostF.Calculate(A_W_minus_epsilon, Y)                    
+                    
+                    dW_app = (L_W_add_epsilon - L_W_minus_epsilon) / (2*epsilon)
+                    
+                    self.layer[l].W = W_backup
+                    self.Train(X, Y)
+                    dW_calc = self.layer[l].dW[i,j]
+                    
+                    delta = dW_calc - dW_app
+                    delta_abs = np.abs(delta)
+                    if delta_abs > delta_max :
+                        delta_max = delta_abs
+                        delta_max_l = l
+                        delta_max_i = i
+                        delta_max_j = j
+                    if delta > 1e-05 :
+                        print("GradientCheck failed; layer[%d]W[%d,%d], delta:%.e" %(l,i,j,delta))
+                        return
+        print("GradientCheck finish, delta_max: layer[%d]W[%d,%d] %e" %(l,i,j,delta_max))
+                       
