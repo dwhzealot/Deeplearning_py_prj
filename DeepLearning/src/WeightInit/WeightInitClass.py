61ee92c149dde4e7b3c442cced25f2cc4e7cb171
dwhzealot <dwhzealot@users.noreply.github.com>
2017-12-09 00:11:03 +0800

    update WeightInitClass.py

--- DeepLearning/src/WeightInit/WeightInitClass.py
+++ DeepLearning/src/WeightInit/WeightInitClass.py
@@ -5,6 +5,12 @@ Created on 2017年12月5日
 @author: DongWenhao
 '''
 import numpy as np
+def Sigmoid_W_init_coe(n):
+    '''
+    n : W的列数，即W的特征个数
+    '''
+    a = 1/n
+    return np.sqrt(a)
 
 def Tanh_W_init_coe(n):
     '''
