e0730e6bda645fc679efeb0cb4183d1d3a4ae831
dwhzealot <33685532+dwhzealot@users.noreply.github.com>
2017-12-19 14:21:13 +0800

    add TF_FullConnClass

--- DeepLearning/src/MyTensorFlow/MNIST_FullConnProc.py
+++ DeepLearning/src/MyTensorFlow/MNIST_FullConnProc.py
@@ -5,8 +5,8 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 from MNIST.GetDataSetClass import *
-from WeightInit.WeightInitClass import *
-from TF_CostFuncClass import *
+from MyTensorFlow.TF_CostFuncClass import *
+from MyTensorFlow.TF_FullConnClass import *
 
 minibatch_size = 512
 test_minibatch_size = 512
@@ -21,44 +21,30 @@ s = 784
 m = minibatch_size
 n = [40, 30, 20, 10]
 learn_rate = 0.1
+epoch = 50
 
 X = tf.placeholder(tf.float32, shape=[s, None])
 Y = tf.placeholder(tf.float32, shape=[10, None])
-l_max = len(n)
-A = 0
 
-for i in range(l_max):
-    if i == 0:
-        column = s
-        input = X
-    else:
-        column = n[i-1]
-        input = A
-    np.random.seed(n[i])
-    W_ini_coe = Sigmoid_W_init_coe(column)
-    W_init = W_ini_coe * (np.random.randn(n[i],column))
-    W_var = tf.Variable(W_init, dtype=tf.float32)
-    b_init = np.zeros((n[i], 1))
-    b_var = tf.Variable(b_init, dtype=tf.float32)
-    
-    Z = tf.matmul(W_var,input) + b_var
-    A = tf.sigmoid(Z)
-    
+A = TF_FullConnForwardPropagation(s, n, X)
+
 Lable = tf.argmax(Y,0)
 rel = tf.argmax(A,0)
 
 CostF = TF_LogisticRegressionCrossEntropy(A, Y, m)
 
 #train_step = tf.train.AdamOptimizer(0.01, 0.9, 0.999).minimize(CostF)
-train_step = tf.train.GradientDescentOptimizer(0.1).minimize(CostF)
+train_step = tf.train.GradientDescentOptimizer(learn_rate).minimize(CostF)
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-for i in range(50):
-    if i%10 == 0:
-        print('epoch',i)
-    
+for i in range(epoch):
+    if i % (epoch // 10) == 0:
+        progress = i / epoch *100
+        print('Training progress: ', progress,'%')
+    if i == (epoch-1):
+        print('Training progress: 100%')
     for j in range(minibatch_num):
         sess.run(train_step, feed_dict={X: TrainDataSet[j], Y: TrainLabelSet[j]}) 
 
---
+++ DeepLearning/src/MyTensorFlow/TF_FullConnClass.py
@@ -0,0 +1,29 @@
+# -*- coding: UTF-8 -*-
+
+import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
+from WeightInit.WeightInitClass import *
+
+def TF_FullConnForwardPropagation(s, n, X):
+    l_max = len(n)
+    A = 0
+    
+    for i in range(l_max):
+        if i == 0:
+            column = s
+            l_input = X
+        else:
+            column = n[i-1]
+            l_input = A
+        np.random.seed(n[i])
+        W_ini_coe = Sigmoid_W_init_coe(column)
+        W_init = W_ini_coe * (np.random.randn(n[i],column))
+        W_var = tf.Variable(W_init, dtype=tf.float32)
+        b_init = np.zeros((n[i], 1))
+        b_var = tf.Variable(b_init, dtype=tf.float32)
+        
+        Z = tf.matmul(W_var,l_input) + b_var
+        A = tf.sigmoid(Z)
+    return A
