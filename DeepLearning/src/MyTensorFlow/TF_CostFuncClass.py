1c33b29345f613a0938c5f759afb354108a430b8
dwhzealot <33685532+dwhzealot@users.noreply.github.com>
2017-12-27 19:22:48 +0800

    update

--- DeepLearning/src/MyTensorFlow/TF_CostFuncClass.py
+++ DeepLearning/src/MyTensorFlow/TF_CostFuncClass.py
@@ -8,9 +8,15 @@ def TF_LogisticRegressionCrossEntropy(A, Y, m):
     L_mat = (-1) * (Y * tf.log(A) + (1-Y) * tf.log(1-A))
     return tf.reduce_sum(L_mat, 1, keep_dims=True) / m
 
+def TF_LogisticRegressionCrossEntropy_2(A, Y, m):
+    L_mat = (-1) * (Y * tf.log(A) + (1-Y) * tf.log(1-A))
+    cost_mat =  tf.reduce_sum(L_mat, 1, keep_dims=True) / m
+    return tf.reduce_sum(cost_mat, 0, keep_dims=True) / 10
+
 def TF_LogisticRegressionCrossEntropy_L2Regularize(A, Y, m, CollectionName):
     L_mat = (-1) * (Y * tf.log(A) + (1-Y) * tf.log(1-A))
-    cost =  tf.reduce_sum(L_mat, 1, keep_dims=True) / m
+    cost_mat =  tf.reduce_sum(L_mat, 1, keep_dims=False) / m
+    cost = tf.reduce_sum(cost_mat, 0, keep_dims=False) / 10
     tf.add_to_collection(CollectionName, cost)
     return tf.add_n(tf.get_collection(CollectionName))
 
--- DeepLearning/src/MyTensorFlow/TF_RegularizeClass.py
+++ DeepLearning/src/MyTensorFlow/TF_RegularizeClass.py
@@ -4,5 +4,5 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 
 def TF_L2RegularizeWeight(tf_W, lamda, CollectionName):
-    tf.add_to_collection(CollectionName, tf.contrib.layers.l2_regularizer(lamda)(tf_W))#把正则化加入集合losses里面
+    tf.add_to_collection(CollectionName, tf.contrib.layers.l2_regularizer(lamda)(tf_W))
     return
