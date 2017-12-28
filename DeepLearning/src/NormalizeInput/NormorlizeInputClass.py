238d209f6f6d03bdefea4ba0f4a25a1c9a6d2f9a
dwhzealot <33685532+dwhzealot@users.noreply.github.com>
2017-12-28 16:52:35 +0800

    change normalize

--- DeepLearning/src/NormalizeInput/NormorlizeInputClass.py
+++ DeepLearning/src/NormalizeInput/NormorlizeInputClass.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 class NormalizeInput (object):
-    def __init__(self, TrainData):
+    def ProcTrainData(self, TrainData):
         m = TrainData.shape[1]
         n = TrainData.shape[0]
         miu = np.sum(TrainData,axis=1, keepdims = True) / m
@@ -12,11 +12,18 @@ class NormalizeInput (object):
         sigmaSquare = np.sum(X_square,axis=1, keepdims = True) / m
         self.sigmaSquare = np.reshape(sigmaSquare, (n,1))
         self.trainDataRet = X_minus_miu / self.sigmaSquare
-    def ProcTrainData(self):
         return self.trainDataRet
+    '''
     def ProcTestData(self, TestData):
         TestData_minus_miu = TestData - self.miu
         return TestData_minus_miu / self.sigmaSquare
+    '''
+
+def NormalizeTestSet(TestSet, miu, sigmaSquare):
+    TestData_minus_miu = TestSet - miu
+    return TestData_minus_miu / sigmaSquare
+   
+   
 '''
 A = np.array([[0,1,2,3,4,5,6,7,8,9],
               [100,101,102,103,104,105,106,107,108,109]]
