12fe716a27b3f390ad9c92ec47e74ca606f08823
dwhzealot <dwhzealot@users.noreply.github.com>
2018-01-16 00:38:01 +0800

    cnn class

---
+++ DeepLearning/src/MyTensorFlow/TF_CnnClass.py
@@ -0,0 +1,63 @@
+# -*- coding: UTF-8 -*-
+
+import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
+
+def ConvPoolLayer(object):
+    def __init__(self, ConvFilter, ConvStrides, ConvPadding, PoolKsize, PoolStrides, PoolPadding, Activator):
+        assert(len(ConvFilter) == 4)
+        assert(len(ConvStrides) == 4)
+        assert(len(PoolKsize) == 4)
+        assert(len(PoolStrides) == 4)        
+        self.ConvFilter = ConvFilter
+        self.ConvStrides = ConvStrides
+        self.ConvPadding = ConvPadding
+        W_init = (1 / ConvFilter[0]) * np.random.randn(ConvFilter[0],ConvFilter[1],ConvFilter[2],ConvFilter[3])
+        self.W = tf.Variable(W_init, dtype=tf.float32)
+        b_init = 0.1 * np.ones(ConvFilter[3])
+        self.b = tf.Variable(b_init, dtype=tf.float32)
+
+        self.PoolKsize = PoolKsize
+        self.PoolStrides = PoolStrides
+        self.PoolPadding = PoolPadding
+        self.Act = Activator
+        return
+    def Calc(self, X):
+        CONV = tf.nn.conv2d(X, self.W, strides = self.ConvStrides, padding = self.ConvPadding) + self.b
+        CONV_A = self.Act(CONV)
+        POOL = tf.nn.max_pool(CONV_A, ksize=self.PoolKsize, strides=self.PoolStrides, padding=self.PoolPadding)
+        return POOL
+
+def ConvPoolNetwork(object):
+    def __init__(self, InputCH, ConvFilterSet, ConvCH, ConvStridesSet, ConvPadSet, 
+                                PoolKsizeSet, PoolStridesSet, PoolPadSet, Activator):
+        self.l_max = len(ConvFilterSet)
+        self.layer = []
+        ConvInputCH = InputCH
+        for i in range(self.l_max):
+            ConvFilter = [ConvFilterSet[i], ConvFilterSet[i],ConvInputCH,ConvCH[i]]
+            ConvStrides = [1, ConvStridesSet[i], ConvStridesSet[i], 1]
+            ConvPadding = ConvPadSet[i]
+            PoolKsize = [1, PoolKsizeSet[i], PoolKsizeSet[i], 1]
+            PoolStrides = [1, PoolStridesSet[i], PoolStridesSet[i], 1]
+            PoolPadding = PoolPadSet[i]
+            layer = ConvPoolLayer(ConvFilter, ConvStrides, ConvPadding,
+                                  PoolKsize, PoolStrides, Activator[i])
+            self.layer.append(layer)
+        return
+    def ForwardPropagation(self, X):
+        A = X
+        for i in range(self.l_max):
+            A = self.layer[i].calc(A)
+        return A              
+
+
+
+
+
+
+
+
+
--- DeepLearning/src/MyTensorFlow/TF_MNIST_CNN/TF_Cnn_simple.py
+++ DeepLearning/src/MyTensorFlow/TF_MNIST_CNN/TF_Cnn_simple.py
@@ -5,12 +5,12 @@ import tensorflow as tf
 from MNIST.GetDataSetClass import *
 
 
-#x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+#x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 x = np.reshape(x, [-1,4,4,1])
 #print(x)
 
-X = tf.placeholder(tf.float32, shape=[1,4,4,1])
+X = tf.placeholder(tf.float32, shape=[2,4,4,1])
 
 W_init = np.ones((2,2,1,1))
 TF_W = tf.Variable(W_init, dtype=tf.float32)
@@ -21,12 +21,12 @@ CONV = tf.nn.conv2d(X, TF_W, strides=[1, 1, 1, 1], padding='SAME') + TF_b_1
 POOL = tf.nn.max_pool(CONV, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 np.random.seed(5)
-W_init_3 = np.random.randn(4,10)
+W_init_3 = np.random.randn(8,10)
 TF_W_3 = tf.Variable(W_init_3, dtype=tf.float32)
 b_init_3 = 0.1 * np.ones(10)
 TF_b_3 = tf.Variable(b_init_3, dtype=tf.float32)
 
-POOL_flat = tf.reshape(POOL, [1, 4])
+POOL_flat = tf.reshape(POOL, [1, 8])
 FC_3 = tf.matmul(POOL_flat, TF_W_3) + TF_b_3
 A_FC_3 = tf.sigmoid(FC_3)
 
@@ -36,14 +36,14 @@ sess.run(tf.global_variables_initializer())
 conv_res = sess.run(CONV, feed_dict={X:x}) 
 print('conv_res shape', conv_res.shape)
 
-conv_res_img = np.reshape(conv_res, (4,4))
+conv_res_img = np.reshape(conv_res, (8,4))
 print('conv_res_img\n',conv_res_img)
 
 pool_res = sess.run(POOL, feed_dict={X:x})
 print('pool_res shape', pool_res.shape)
 
 
-pool_res_img = np.reshape(pool_res, (2,2))
+pool_res_img = np.reshape(pool_res, (4,2))
 print('pool_res_img\n',pool_res_img)
 
 fc_res = sess.run(A_FC_3, feed_dict={X:x})
@@ -52,6 +52,7 @@ print(fc_res)
 
 sess.close()
 '''
+m =1
 conv_res shape (1, 4, 4, 1)
 conv_res_img
  [[ 15.  19.  23.  13.]
@@ -69,4 +70,27 @@ pool_res shape (1, 10)
     
 '''
 
+'''
+m =2
+conv_res shape (2, 4, 4, 1)
+conv_res_img
+ [[ 15.  19.  23.  13.]
+ [ 31.  35.  39.  21.]
+ [ 47.  51.  55.  29.]
+ [ 28.  30.  32.  17.]
+ [ 15.  19.  23.  13.]
+ [ 31.  35.  39.  21.]
+ [ 47.  51.  55.  29.]
+ [ 28.  30.  32.  17.]]
+pool_res shape (2, 2, 2, 1)
+pool_res_img
+ [[ 35.  39.]
+ [ 51.  55.]
+ [ 35.  39.]
+ [ 51.  55.]]
+pool_res shape (1, 10)
+[[  0.00000000e+00   1.00000000e+00   1.00000000e+00   0.00000000e+00
+    2.58687983e-27   1.00000000e+00   1.00000000e+00   1.00000000e+00
+    0.00000000e+00   1.00000000e+00]]
+'''
 
