4b6429dc72902990bc2acbc4e644835f4a05f63a
dwhzealot <dwhzealot@users.noreply.github.com>
2018-01-18 23:48:06 +0800

    cnn

--- DeepLearning/src/MyTensorFlow/TF_MNIST_CNN/TF_CnnProc.py
+++ DeepLearning/src/MyTensorFlow/TF_MNIST_CNN/TF_CnnProc.py
@@ -1,8 +1,12 @@
+# -*- coding: UTF-8 -*-
 import numpy as np
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 from MyTensorFlow.TF_CnnClass import *
+from MyTensorFlow.TF_CostFuncClass import *
+from MyTensorFlow.TF_FullConnClass import *
+
 
 #x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 #x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
@@ -23,14 +27,32 @@ PoolPadSet = ['SAME','SAME']
 act = tf.nn.relu
 Activator = [act, act]
 CP_Layer = ConvPoolNetwork(1, ConvFilterSet, ConvCH, ConvStridesSet, ConvPadSet, PoolKsizeSet, PoolStridesSet, PoolPadSet, Activator)
-A =  CP_Layer.ForwardPropagation(X)
+CP_A =  CP_Layer.ForwardPropagation(X)
+
+'''
+把卷基层输出的结果一维化
+'''
+CP_A_SHAPE = tf.shape(CP_A)
+CP_A_FLAT_LEN = CP_A_SHAPE[1] * CP_A_SHAPE[2] * CP_A_SHAPE[3]
+CP_A_FLAT = tf.reshape(CP_A, [-1, CP_A_FLAT_LEN])
+
+'''
+把一维化的结果列化，即每一列数据对应一个样本
+'''
+FC_INPUT = tf.transpose(CP_A_FLAT)
+n = [10]
+learn_rate = 0.1
+epoch = 80
+FC_Activator = tf.sigmoid
+
+FC_A = TF_FullConnForwardPropagation(4, n, FC_INPUT, FC_Activator)
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-y = sess.run(A, feed_dict={X: x})
+y = sess.run(FC_A, feed_dict={X: x})
 print(y.shape)
-print(y)
+#print(y)
 
 sess.close()
 
---
+++ DeepLearning/src/MyTensorFlow/TF_WeightInitClass.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+
+import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
+
+def TF_Sigmoid_FC_W_init_coe(n):
+    '''
+    n : W的列数，即W的特征个数
+    '''
+    a = tf.div(1,n)
+    return tf.sqrt(a)
